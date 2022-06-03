1f850b4038369bbda90f40768bb0020a95a25385
kai <kai@kaiconsulting.co>
2022-06-03 15:37:46 -0600

    added left part of nav bar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,26 @@
 import './topbar.scss'
+import { Person, Email } from '@mui/icons-material';
 
 export default function Topbar() {
-  return (
-    <div className="topbar">
-
-    </div>
-  )
+    return (
+        <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">genius.</a>
+                    <div className="itemContainer">
+                        <Person className="icon"/>
+                        <span>+44 924 12 74</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Email className="icon"/>
+                        <span>kai@kaiconsulting.co</span>
+                    </div>
+                </div>
+                <h1></h1>
+                <div className="right">
+                    
+                </div>
+            </div>
+        </div>
+    )
 }
